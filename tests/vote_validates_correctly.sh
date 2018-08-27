c2efc256c64ea852b82930e206c84d53b462c444
Matthieu Vachon <mvachon@samsao.co>
2018-08-27 10:21:53 -0400

    Updated implementation for new proposal
    * Added expires_at (with max 6 months) to propose
    * Added created_at to proposal table
    * Renamed cleanvotes to clnproposal
    * Added grace period within which the proposal + votes cannot be cleaned
    * Added verification that vote can happen only on non-expired proposal
    * Added verification that unvote can happen only on active or expired +
      outside grace period.

--- tests/vote_validates_correctly.sh
+++ tests/vote_validates_correctly.sh
@@ -6,24 +6,27 @@ vote_json_not_object="[]"
 vote_json_too_long="{\\\\"a\\\\":\\\\"${CHARS_13000}\\\\"}"
 
 action_ok propose proposer1@active \
-'{"proposer":"proposer1", "proposal_name":"votevalcor2", "title":"A simple one", "proposal_json":null}'
+"{\"proposer\":\"proposer1\", \"proposal_name\":\"votevalcor4\", \"title\":\"A simple one\", \"proposal_json\":null, \"expires_at\":\"${EXPIRES_AT}\"}"
 
 action_ko vote voter1@active \
-'{"voter":"voter2","proposer":"proposer1","proposal_name":"votevalcor2","proposal_hash":"","vote":0,"vote_json":""}' \
+'{"voter":"voter2","proposer":"proposer1","proposal_name":"votevalcor4","vote":0,"vote_json":""}' \
 'missing authority of voter2'
 
 action_ko vote voter1@active \
-"{\"voter\":\"voter1\",\"proposer\":\"proposer1\",\"proposal_name\":\"notexist\",\"proposal_hash\":\"\",\"vote\":0,\"vote_json\":\"\"}" \
-"proposal_name does not exist under proposer's scope"
+"{\"voter\":\"voter1\",\"proposer\":\"proposer1\",\"proposal_name\":\"notexist\",,\"vote\":0,\"vote_json\":\"\"}" \
+"proposal_name does not exist under proposer's scope."
 
 action_ko vote voter1@active \
-"{\"voter\":\"voter1\",\"proposer\":\"proposer1\",\"proposal_name\":\"votevalcor2\",\"proposal_hash\":\"${CHARS_250}\",\"vote\":0,\"vote_json\":\"\"}" \
-'proposal_hash should be less than 128 characters long.'
+"{\"voter\":\"voter1\",\"proposer\":\"proposer1\",\"proposal_name\":\"votevalcor4\",,\"vote\":0,\"vote_json\":\"${vote_json_not_object}\"}" \
+'vote_json must be a JSON object (if specified).'
 
 action_ko vote voter1@active \
-"{\"voter\":\"voter1\",\"proposer\":\"proposer1\",\"proposal_name\":\"votevalcor2\",\"proposal_hash\":\"\",\"vote\":0,\"vote_json\":\"${vote_json_not_object}\"}" \
-'vote_json must be a JSON object (if specified)'
+"{\"voter\":\"voter1\",\"proposer\":\"proposer1\",\"proposal_name\":\"votevalcor4\",,\"vote\":0,\"vote_json\":\"${vote_json_too_long}\"}" \
+'vote_json should be shorter than 8192 bytes.'
+
+action_ok expire proposer1@active \
+'{"proposer":"proposer1", "proposal_name":"votevalcor4"}'
 
 action_ko vote voter1@active \
-"{\"voter\":\"voter1\",\"proposer\":\"proposer1\",\"proposal_name\":\"votevalcor2\",\"proposal_hash\":\"\",\"vote\":0,\"vote_json\":\"${vote_json_too_long}\"}" \
-'vote_json should be shorter than 8192 bytes'
+'{"voter":"voter1","proposer":"proposer1","proposal_name":"votevalcor4","vote":0,"vote_json":""}' \
+'cannot vote on an expired proposal.'
